9474feeb06dd43a442a4b23ef0d46cdd7a3dc83f
Lavish Thakkar <lavisht22@gmail.com>
2024-10-22 10:57:44 +0530

    UI for images and text parts

--- src/routes/(app)/(workspace)/prompt-details/components/user-message.tsx
+++ src/routes/(app)/(workspace)/prompt-details/components/user-message.tsx
@@ -1,5 +1,15 @@
-import { Button, Card, CardBody, Chip, cn, Textarea } from "@nextui-org/react";
-import { LuTrash2 } from "react-icons/lu";
+import {
+  Button,
+  Card,
+  CardBody,
+  Chip,
+  cn,
+  Input,
+  Radio,
+  RadioGroup,
+  Textarea,
+} from "@nextui-org/react";
+import { LuImagePlus, LuText, LuTrash2 } from "react-icons/lu";
 import { z } from "zod";
 import VariablesList from "./variables-list";
 
@@ -40,45 +50,132 @@ export default function UserMessage({
 }) {
   return (
     <Card className={cn(isInvalid && "bg-danger-50")}>
-      <CardBody className="gap-4">
+      <CardBody className="gap-0">
         <Chip size="sm" variant="flat" color="primary">
           USER
         </Chip>
 
-        {value.content.map((part, index) => {
-          if (part.type === "text") {
-            return (
-              <div key={index}>
-                <Textarea
-                  className="mb-2"
-                  aria-label="User message"
-                  variant="bordered"
-                  placeholder="Enter user message..."
-                  value={part.text}
-                  onChange={(e) => {
-                    const newContent = [...value.content];
-                    newContent[index] = { type: "text", text: e.target.value };
-                    onValueChange({
-                      ...value,
-                      content: newContent,
-                    });
-                  }}
-                  minRows={3}
-                  maxRows={100000}
-                />
-                <VariablesList
-                  text={part.text}
-                  variableValues={variableValues}
-                  openVariablesDialog={openVariablesDialog}
-                />
-              </div>
-            );
-          }
+        <div className="mb-4">
+          {value.content.map((part, index) => {
+            if (part.type === "text") {
+              return (
+                <div key={index} className="py-4 border-b-2 border-dashed">
+                  <Textarea
+                    aria-label="User message"
+                    variant="bordered"
+                    placeholder="Enter text"
+                    value={part.text}
+                    onChange={(e) => {
+                      const newContent = [...value.content];
+                      newContent[index] = {
+                        type: "text",
+                        text: e.target.value,
+                      };
+                      onValueChange({
+                        ...value,
+                        content: newContent,
+                      });
+                    }}
+                    minRows={3}
+                    maxRows={100000}
+                  />
+                  <VariablesList
+                    text={part.text}
+                    variableValues={variableValues}
+                    openVariablesDialog={openVariablesDialog}
+                  />
+                </div>
+              );
+            }
 
-          if (part.type === "image_url") {
-            return <div key={index}>Coming soon...</div>;
-          }
-        })}
+            if (part.type === "image_url") {
+              return (
+                <div
+                  key={index}
+                  className="py-4 border-b-2 border-dashed flex flex-col gap-2"
+                >
+                  <Input
+                    label="URL"
+                    value={part.image_url.url}
+                    onChange={(e) => {
+                      const newContent = [...value.content];
+                      newContent[index] = {
+                        type: "image_url",
+                        image_url: {
+                          url: e.target.value,
+                          detail: part.image_url.detail,
+                        },
+                      };
+                      onValueChange({
+                        ...value,
+                        content: newContent,
+                      });
+                    }}
+                  />
+
+                  <RadioGroup
+                    className="px-2"
+                    size="sm"
+                    label="Detail"
+                    orientation="horizontal"
+                    value={part.image_url.detail}
+                    onValueChange={(newValue: string) => {
+                      const newContent = [...value.content];
+                      newContent[index] = {
+                        ...part,
+                        image_url: {
+                          ...part.image_url,
+                          detail: newValue as "auto" | "low" | "high",
+                        },
+                      };
+                      onValueChange({ ...value, content: newContent });
+                    }}
+                  >
+                    <Radio value="auto">Auto</Radio>
+                    <Radio value="low">Low</Radio>
+                    <Radio value="high">High</Radio>
+                  </RadioGroup>
+                </div>
+              );
+            }
+          })}
+        </div>
+
+        <div className="flex flex-row gap-2">
+          <Button
+            variant="flat"
+            size="sm"
+            startContent={<LuImagePlus className="size-4" />}
+            onPress={() => {
+              const newContent = [...value.content];
+              newContent.push({
+                type: "image_url",
+                image_url: { url: "", detail: "auto" },
+              });
+              onValueChange({
+                ...value,
+                content: newContent,
+              });
+            }}
+          >
+            Add Image Part
+          </Button>
+          <Button
+            variant="flat"
+            size="sm"
+            startContent={<LuText className="size-4" />}
+            onPress={() => {
+              const newContent = [...value.content];
+              newContent.push({ type: "text", text: "" });
+              onValueChange({
+                ...value,
+                content: newContent,
+              });
+            }}
+          >
+            Add Text Part
+          </Button>
+        </div>
 
         <div className="absolute top-0 right-0">
           <Button
